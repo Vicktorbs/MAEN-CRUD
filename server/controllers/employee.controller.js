9e334e47eb1a79b2ed8ea699a732c2df2a204f61
Victor Bolaños Solís <viktorb.132@gmail.com>
2018-12-13 09:55:37 -0600

    Rutas del servidor

---
+++ server/controllers/employee.controller.js
@@ -0,0 +1,48 @@
+// Modelos de los datos, para hacer las consultas a la DB
+const Employee = require('../models/employee');
+// Definicion del objeto
+const employeeCtrl = {};
+
+// Metodos
+// Obtiene todos los empleados
+employeeCtrl.getEmployees = async (red, res) => { 
+    const employees = await Employee.find()
+    // Respuesta al navegador
+    res.json(employees);
+};
+// Agrega un nuevo registro del empleado
+employeeCtrl.createEmployee = async (req, res) => {
+    // Guardar los datos
+    const employee = new Employee(req.body);
+    await employee.save();
+    res.json({
+        'status': 'Employee saved'
+    });
+};
+// Obtiene solo un empleado
+employeeCtrl.getEmployee = async (req, res) => {
+    const employee = await Employee.findById(req.params.id);
+    res.json(employee)
+}
+// Editar un empleado
+employeeCtrl.editEmployee = async (req, res) => {
+    const { id } = req.params;
+    const employee = {
+        name: req.body.name,
+        activity: req.body.activity,
+        production: req.body.production
+    };
+    await Employee.findByIdAndUpdate(id, { $set: employee}, { new: true });
+    res.json({
+        status: 'Employee update'
+    });
+}
+// Borrar el registro de un empleado
+employeeCtrl.deleteEmployee = async (req, res) => {
+    await Employee.findByIdAndRemove(req.params.id);
+    res.json({
+        status: 'Employed deleted'
+    })
+}
+
+module.exports = employeeCtrl;
--- server/routes/employee.routes.js
+++ server/routes/employee.routes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const emploCtrl = require('../controllers/employee.controller')
 
 // Ruta para enviar y recibir informacion
-router.get('/', (red, res) => {
-    res.send({
-        status: 'API Works'
-    });
-})
+router.get('/', emploCtrl.getEmployees);
+router.post('/', emploCtrl.createEmployee);
+router.get('/:id', emploCtrl.getEmployee);
+router.put('/:id', emploCtrl.editEmployee);
+router.delete('/:id', emploCtrl.deleteEmployee);
 
 module.exports = router;
